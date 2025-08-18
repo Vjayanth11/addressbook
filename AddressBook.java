37077385e1499d48a07f2042e476edad52c89f5c
Vjayanth11 <126314010+Vjayanth11@users.noreply.github.com>
2025-08-18 15:43:55 +0530

    thirdcommit

--- AddressBook.java
+++ AddressBook.java
@@ -11,6 +11,29 @@ public class AddressBook {
         System.out.println("Contact added successfully!");
     }
 
+    public void editContact(String firstName, Contact updatedContact) {
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).getFirstName().equalsIgnoreCase(firstName)) {
+                contacts.set(i, updatedContact);
+                System.out.println("Contact updated successfully!");
+                return;
+            }
+        }
+        System.out.println("Contact not found.");
+    }
+
+    // 🔹 UC3: Delete a contact by first name
+    public void deleteContact(String firstName) {
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).getFirstName().equalsIgnoreCase(firstName)) {
+                contacts.remove(i);
+                System.out.println("Contact deleted successfully!");
+                return;
+            }
+        }
+        System.out.println("Contact not found.");
+    }
+
     public void showContacts() {
         if (contacts.isEmpty()) {
             System.out.println("No contacts in address book.");
--- Contact.java
+++ Contact.java
@@ -1,6 +1,5 @@
 package com.addressbook;
 
-
 public class Contact {
     private String firstName;
     private String lastName;
@@ -20,6 +19,70 @@ public class Contact {
         this.state = state;
         this.zip = zip;
         this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 
